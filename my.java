c92567cdcdbfba2f2b9ce16b744e59efce88f058
Babasaheb Khedkar <khedkarbabasaheb21@gmail.com>
2024-10-23 08:50:16 +0530

    added new file

---
+++ my.java
@@ -0,0 +1,10 @@
+class Hello {
+    public static void main(String[] args){
+        String name = "Babasaheb Khedkar";
+        String mobileNumber = "+91 8677882785";
+
+
+        System.out.println("Your Name: " + name);
+        System.out.println("Your Mobile Number: " + mobileNumber);
+    }
+}
